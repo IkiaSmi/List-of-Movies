452e078751d685e4e42a3ccdd3f04441c0d71dd0
user <iluxeo6@gmail.com>
2023-10-01 09:59:39 +0300

    First commit

---
+++ src/main/java/org/example/ListOfMoviesApplication.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListOfMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListOfMoviesApplication.class, args);
+    }
+
+}
